464b856a58e8f7ce3e5f59d172d6e2df5ae2dc83
memoriesdead <98496589+memoriesdead@users.noreply.github.com>
2022-12-29 06:39:23 -0800

    Update base.py

--- splearn/feature_selection/base.py
+++ splearn/feature_selection/base.py
@@ -21,4 +21,22 @@ class SparkSelectorMixin(six.with_metaclass(ABCMeta, SelectorMixin,
     `_get_support_mask`.
     """
 
-    pass
+    def __init__(self, max_features=None):
+        self.max_features = max_features
+
+    def fit(self, X, y=None):
+        """Fit the feature selector to the training data
+
+        Parameters
+        ----------
+        X : array-like or RDD
+            Training data.
+        y : array-like, optional
+            Target labels.
+
+        Returns
+        -------
+        self
+        """
+        self._fit(X, y)
+        return self
